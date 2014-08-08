4446d76aa0d307df72ef7b722b63fa3ad9442112
kezhai <kezhai@yahoo-inc.com>
2014-08-08 12:07:52 -0400

    update

--- src/util/plot_likelihood.R
+++ src/util/plot_likelihood.R
@@ -9,10 +9,9 @@ library(ggplot2)
 library(grid)
 library(scales)
 
-#file_name="140730-224558-cluster-counts"
-file_name="140806-165121-cluster-counts"
+file_name="140806-165121-log-likelihood"
 
-#project_home="/windows/d/Workspace/PyHDP/"
+#project="/windows/d/Workspace/OnlineLDA/"
 project_home="/Users/kezhai/Workspace/PyHDP/"
 
 input_directory=paste(project_home, "result/", sep="");
@@ -21,26 +20,26 @@ output_directory=paste(project_home, "figure/", sep="");
 input_file=paste(input_directory, file_name, ".csv", sep="");
 output_file=paste(output_directory, file_name, ".pdf", sep="");
 
-pdf(width=8, height=8)
+pdf(width=6, height=2.8)
 
 # load in csv data
 input_data <- read.csv(input_file)
+#input_data <- subset(input_data, input_data$iteration<=500)
 
-plot_pic <- qplot(vocabulary, factor(topic), data=input_data, size=count) +
-	facet_grid(inference ~ ., scales="free", space="free") +
-	labs(size="Tokens Count") +
-	labs(x="Vocabulary", y="Topic") +
-
+plot_pic <- ggplot() +
+	geom_line(data=input_data, aes(x=iteration, y=likelihood, color=inference, group=inference), alpha=0.75, size=1) +
+	labs(x="Iteration", y="Log-Likelihood") +
+	
+	#guides(colour = guide_legend(nrow = 3)) +
+	
 	#scale_x_log10() +
-
+	
 	theme(legend.margin = unit(0, "line")) +
-	theme(legend.key.height=unit(1.5,"line")) +
+	theme(legend.key.height=unit(1,"line")) +
 	theme(legend.position="bottom") +
-	theme(legend.title = element_text(size = 12, angle = 0), legend.text = element_text(size = 15)) +
-	guides(colour = guide_legend(nrow = 1)) +
-		
-	coord_cartesian(xlim=c(0.5, 25.5)) +
-	#coord_cartesian(ylim=c(0, 13, 2)) +
+	theme(legend.title = element_text(size = 0, angle = 90), legend.text = element_text(size = 13)) +
+	
+	coord_cartesian(ylim=c(-45000,-41500)) +
 	#scale_y_continuous(breaks = round(seq(-6000, -4000, by=1000), 1)) +
 	#scale_x_continuous(breaks = round(seq(1, 500, by=100), 1)) + 
 	
@@ -48,7 +47,7 @@ plot_pic <- qplot(vocabulary, factor(topic), data=input_data, size=count) +
 	#theme(legend.direction='vertical', legend.box='vertical', legend.position = c(0, 0), legend.justification = c(0, 1)) +
 	theme(axis.text.x = element_text(size = 15, colour = "black")) +
 	theme(axis.text.y = element_text(size = 15, colour = "black")) +
-	theme(axis.title.x = element_text(size = 12), axis.title.y = element_text(size = 15, angle = 90)) +
-	theme(strip.text.x = element_text(size = 15), strip.text.y = element_text(size = 15, angle = -90))
+	theme(axis.title.x = element_text(size = 15), axis.title.y = element_text(size = 15, angle = 90)) +
+	theme(strip.text.x = element_text(size = 15), strip.text.y = element_text(size = 15, angle = 45))
 					
 ggsave(plot_pic,filename=output_file);

97c26fd061325c9f78000ce4e6d7fe3340ac9378
Ke Zhai <kzhai@kzhai-desktop.(none)>
2014-09-10 15:58:17 -0700

    update

--- src/util/generate_data.py
+++ src/util/generate_data.py
@@ -8,7 +8,7 @@ import os;
 def parse_args():
     parser = optparse.OptionParser()
     parser.set_defaults(number_of_topics=10,
-                        number_of_vocabulary=100,
+                        number_of_vocabularies=100,
                         number_of_documents=1000,                        
                         output_directory=None,
                         asymmetric_alpha_prior=False,
@@ -16,7 +16,7 @@ def parse_args():
                             )
     parser.add_option("--number_of_topics", type="int", dest="number_of_topics",
                       help="number of topics [10]");
-    parser.add_option("--number_of_vocabulary", type="int", dest="number_of_vocabulary",
+    parser.add_option("--number_of_vocabularies", type="int", dest="number_of_vocabularies",
                       help="number of types per topic [10]");
     parser.add_option("--number_of_documents", type="int", dest="number_of_documents",
                       help="number of documents [1000]");
@@ -40,7 +40,7 @@ def main():
     number_of_documents = options.number_of_documents;
     asymmetric_alpha_prior = options.asymmetric_alpha_prior;
     
-    number_of_vocabularies = options.number_of_vocabulary;
+    number_of_vocabularies = options.number_of_vocabularies;
     number_of_tokens_per_document = options.number_of_tokens_per_document;
     
     now = datetime.datetime.now();
@@ -65,12 +65,12 @@ def main():
         output_file_stream.write("%d\n" % (type_id));
     output_file_stream.close();
     
-    beta = 1.0/number_of_vocabularies;
+    beta = numpy.zeros(number_of_vocabularies) + 1.0/number_of_vocabularies;
     topic_vocabulary_probability = numpy.zeros((number_of_topics, number_of_vocabularies));
     
     output_file_stream = open(os.path.join(output_directory, "topic.dat"), 'w');
     for topic_index in xrange(number_of_topics):
-        topic_vocabulary_probability[topic_index, :] = numpy.random.dirichlet(beta, number_of_vocabularies);
+        topic_vocabulary_probability[topic_index, :] = numpy.random.dirichlet(beta);
         
         output_file_stream.write("==========\t%d\t==========\n" % (topic_index));
         for type_index in xrange(number_of_vocabularies):
@@ -79,7 +79,7 @@ def main():
     output_file_stream = open(os.path.join(output_directory, "doc.dat"), 'w');
     for doc_id in xrange(number_of_documents):
         doc = [];
-        topic_distribution = numpy.random.dirichlet(alpha, number_of_topics);
+        topic_distribution = numpy.random.dirichlet(alpha);
         for token_id in xrange(numpy.random.poisson(number_of_tokens_per_document)):
             topic_id = numpy.nonzero(numpy.random.multinomial(1, topic_distribution))[0][0];
             type_id = numpy.nonzero(numpy.random.multinomial(1, topic_vocabulary_probability[topic_id, :]))[0][0];

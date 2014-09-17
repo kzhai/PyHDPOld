f2ed63734abb72aab6e2df5d665d2d66241ddd4c
Ke Zhai <kzhai@kzhai-desktop.(none)>
2014-09-17 12:48:15 -0700

    update

--- src/util/plot_statistics.R
+++ src/util/plot_statistics.R
@@ -41,13 +41,12 @@ pdf(width=8, height=5)
 # load in csv data
 input_data <- read.csv(input_file)
 
-my_function<-function(x){data.frame(ymin=mean(x)-sd(x),ymax=mean(x)+sd(x),y=mean(x))}
+#my_function<-function(x){data.frame(ymin=mean(x)-sd(x),ymax=mean(x)+sd(x),y=mean(x))}
 #dodge = position_dodge(width=0.9)
 
-plot_pic <- ggplot(data=input_data, aes(x=factor(inference), y=value)) +
-	stat_summary(fun.y = mean, geom = "bar", position = "dodge", alpha=0.5) +
-	stat_summary(fun.data = my_function, geom = "pointrange", position = position_dodge(width = 0.90)) 	 
-	
+#plot_pic <- ggplot(data=input_data, aes(x=factor(inference), y=value)) +
+	#stat_summary(fun.y = mean, geom = "bar", position = "dodge", alpha=0.5) +
+	#stat_summary(fun.data = my_function, geom = "pointrange", position = position_dodge(width = 0.90)) 	 
   	
 plot_pic <- ggplot(data=input_data, aes(x=factor(inference),  y=value, lower='25%', middle='50%', upper='75%')) +
 	geom_boxplot() +
